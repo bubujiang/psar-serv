ad12e6ecb14ae226016d09d0a18347ec826788b2
x <x>
2020-06-08 03:01:04 +0800

    界面基本结构完成, websockets通信通过.

--- action.go
+++ action.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"net/http"
 )
 
 func Index (con *gin.Context, hosts map[string]string) {
 	k:=""
 	for k = range hosts{ break }
+	host := con.DefaultQuery("h",k)
+	hostUrl := hosts[host]
 
 	showData := gin.H{
 		"hosts": hosts,
-		"host": con.DefaultQuery("h",k),
+		"host": host,
+		"hostUrl": template.URL(hostUrl),
 	}
 
 	con.HTML(http.StatusOK, "index.html", showData)
